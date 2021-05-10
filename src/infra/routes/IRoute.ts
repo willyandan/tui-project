c0e4fc1a9a65498fa4c6578cd9b42d86adb09c8b
Willyan Antunes <willyan.dantunes@gmail.com>
2021-05-10 15:26:45 -0300

    Melhorando rotas

--- src/infra/routes/IRoute.ts
+++ src/infra/routes/IRoute.ts
@@ -1,11 +1,12 @@
 import { HttpMethods } from "../../domain/entity/request/HttpMethods";
+import { Request } from "../../domain/entity/request/Request";
 import { IResponse } from "../../domain/entity/response/IResponse";
+import { IMiddleware } from "../middleware/IMiddleware";
 
 export interface IRoute<Response> {
 	path: string;
 	method: HttpMethods;
-	query:Record<string, string | number | Array<string | number>>
-	body:Record<string, any>
-	params:Record<string, string>
+	request:Request
+	middlewares:Array<IMiddleware>
 	execute(): Promise<IResponse<Response>>
 }
--- src/infra/routes/impl/BaseRoute.ts
+++ src/infra/routes/impl/BaseRoute.ts
@@ -1,37 +1,44 @@
-import { plainToClass, plainToClassFromExist } from "class-transformer";
-import { injectable, named } from "inversify";
+import { ClassConstructor, plainToClass, plainToClassFromExist } from "class-transformer";
+import { injectable } from "inversify";
 import { HttpMethods } from "../../../domain/entity/request/HttpMethods";
 import { IResponse } from "../../../domain/entity/response/IResponse";
 import { IRoute } from "../IRoute";
+import { Request } from "../../../domain/entity/request/Request";
+import { validate, validateOrReject, ValidationError } from "class-validator";
+import { IMiddleware } from "../../middleware/IMiddleware";
+import { BadRequestException } from "../../../domain/entity/exceptions/BadRequestException";
 
 @injectable()
-export abstract class BaseRoute<Request, Response> implements IRoute<Response> {
+export abstract class BaseRoute<Response> implements IRoute<Response> {
 	path = '/'
-	query:Record<string, string | number | Array<string|number>> = {}
-	body:Record<string, any> = {}
-	params:Record<string, string> = {}
 	method = HttpMethods.GET;
-	request?:Request;	
+	request!:Request;	
+	middlewares:Array<IMiddleware> = []
 
-	validate(request?:Request){
-		this.request = plainToClassFromExist(request,{
-			...this.body,
-			...this.query,
-			...this.params
-
-		})	
+	prepareRequest(body:Record<string, any>, query:Record<string, any>, params:Record<string, string>){
+		this.request = plainToClass(Request,{body,query,params})
 	}
 
-	parseRequest(
-		query:Record<string, any>, 
-		body:Record<string,any>, 
-		params:Record<string,string>
-	){
-		this.body = body
-		this.query = query
-		this.params = params
+	private async validateClass(cls:object):Promise<void>{
+		try{
+			await validateOrReject(cls)
+		}
+		catch(err){
+			const errorData = err as Array<ValidationError>
+			throw new BadRequestException('Bad request!',{errors:errorData})
+		}
 	}
 
+	async requestToUseCaseDto<T extends object>(cls:ClassConstructor<T>):Promise<T>{
+		const dto =  plainToClass(cls, {
+			...this.request.body,
+			...this.request.query,
+			...this.request.params
+		})
+		await this.validateClass(dto)
+		return dto;
+	}
+	
 	execute():Promise<IResponse<Response>>{
 		throw new Error('Method not implemented')	
 	}	
