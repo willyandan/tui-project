c674acc7b8f8b38130773f560bbea63072bd7774
Willyan Antunes <willyan.dantunes@gmail.com>
2021-05-10 15:30:23 -0300

    Adicionado novos metodos no hotelRepository

--- src/infra/repository/HotelRepository/HotelRepositoryMongooseImpl.ts
+++ src/infra/repository/HotelRepository/HotelRepositoryMongooseImpl.ts
@@ -1,14 +1,37 @@
 import { injectable } from "inversify";
 import { Hotels, IHotel } from "../../../domain/entity/Hotel";
 import { PaginatedResponse } from "../../../domain/entity/response/PaginatedResponse";
+import { SearchHotelsDto } from "../../../domain/useCase/SearchHotelUseCase/SearchHotelsDTO";
 import { IHotelRepository } from "./IHotelRepository";
 
 @injectable()
 export class HotelRespositoryMongooseImpl implements IHotelRepository{
-	async search(){
-		const hotels = await Hotels.find({}).limit(10).skip(0)
-
-		const paginatedHotels = new PaginatedResponse<IHotel>(hotels,1,10)
+	async search(params:SearchHotelsDto){
+		const query = Hotels.find()		
+		if(params.cityCode){
+			query.where('hotel.cityCode',params.cityCode)
+		}else if(params.lat && params.lng){
+			const radius = params.radius ?? 5
+			query.where('location').within({
+				center: [params.lng, params.lat], radius: radius/6371, unique: true, spherical: true 
+			})
+		}
+		let asc = params.asc
+		if(asc === undefined){
+			asc = true
+		}
+		query.sort({'offers.0.price.base':asc?1:-1})
+		const hotels = await query.limit(params.pageSize).skip((params.page - 1) * params.pageSize)
+	
+		const paginatedHotels = new PaginatedResponse<IHotel>(hotels,params.page,params.pageSize)
 		return paginatedHotels;
+	}
+	
+	async getById(id:string){
+		const hotel = await Hotels.findById(id)
+		if(!hotel){
+			throw new Error('hotel not found')
+		}
+		return hotel;
 	}
 }
--- src/infra/repository/HotelRepository/IHotelRepository.ts
+++ src/infra/repository/HotelRepository/IHotelRepository.ts
@@ -1,6 +1,8 @@
+import { IHotel } from "../../../domain/entity/Hotel";
 import { PaginatedResponse } from "../../../domain/entity/response/PaginatedResponse";
+import { SearchHotelsDto } from "../../../domain/useCase/SearchHotelUseCase/SearchHotelsDTO";
 
 export interface IHotelRepository {
-	search():Promise<PaginatedResponse<IHotel>>
-
+	search(params:SearchHotelsDto):Promise<PaginatedResponse<IHotel>>
+	getById(id:string):Promise<IHotel>
 }
