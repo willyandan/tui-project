eb6259d84ddd4b0ee4d392e8f6f508513899477a
Willyan Antunes <willyan.dantunes@gmail.com>
2021-05-10 15:39:07 -0300

    adicionado useCase e rotas no inversify

--- config/inversify.ts
+++ config/inversify.ts
@@ -1,13 +1,17 @@
 import { Container } from "inversify";
-import { SearchHotelUseCase } from "../src/domain/useCase/SearchHotelUseCase";
+import { GetHotelByUidUseCase } from "../src/domain/useCase/GetHotelByUidUseCase/GetHotelByUidUseCase";
+import { SearchHotelUseCase } from "../src/domain/useCase/SearchHotelUseCase/SearchHotelUseCase";
 import { HotelRespositoryMongooseImpl } from "../src/infra/repository/HotelRepository/HotelRepositoryMongooseImpl";
+import { GetHotelByUid } from "../src/infra/routes/impl/GetHotelByUid";
 import { SearchHotels } from "../src/infra/routes/impl/SearchHotels";
 import { IHotelRepository, IRoute } from "./types";
 
 export const container = new Container()
 
 container.bind(IRoute).to(SearchHotels)
+container.bind(IRoute).to(GetHotelByUid)
 
 container.bind(IHotelRepository).to(HotelRespositoryMongooseImpl)
 
 container.bind(SearchHotelUseCase).toSelf()
+container.bind(GetHotelByUidUseCase).toSelf()
